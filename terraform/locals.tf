d75bec1f8c1f4a5df9a79d9a0329bb8e02f140d5
Ryan Davis <rdavis542@gmail.com>
2026-05-12 18:17:40 -0400

    Fix deprecated data.aws_region.current.name -> .region

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  region     = data.aws_region.current.name
+  region     = data.aws_region.current.region
   account_id = data.aws_caller_identity.current.account_id
   name       = "ec2-toolbox"
 }
